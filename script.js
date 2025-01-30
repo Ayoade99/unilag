407a105f10fd56dfaae7ee8eba8c758f570ad15e
Ayoade Idris <ayoadeidris01@gmail.com>
2025-01-30 12:29:55 +0100

    responsiveness and bar-icon for smaller screens

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const barIcon = document.querySelector("#bars").addEventListener("click", toggleBar);
+
+function toggleBar() {
+    
+const menuLinks = document.querySelector(".nav-links");
+menuLinks.classList.toggle("show");
+};
